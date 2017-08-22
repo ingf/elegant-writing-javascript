e0840920d90e3512b22dcca2c45546a456b1cb03
Feng <ingf.ivan@gmail.com>
2017-08-22 16:17:29 +0800

    :sparkles: add server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//index.js
-//require `babel/register` to handle JavaScript code
+// index.js
+// require `babel/register` to handle JavaScript code
 // require('babel/register');
 require("babel-register");
 require('./server.js');
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-//server.js
-import express from 'express';
+// server.js
+import express from 'express'
 
-let app = express();
-let PORT = 3000;
+const app = express()
+const PORT = 3000
 
 app.post('/', (req, res) => {
-  res.send('Hello!');
-});
+  res.send('Hello!')
+})
 
-let server = app.listen(PORT, function() {
-  let host = server.address().address;
-  let port = server.address().port;
+const server = app.listen(PORT, () => {
+  const host = server.address().address
+  const port = server.address().port
 
-  console.log('Listening at http://%s:%s', host, port);
-});
+  console.log('Listening at http://%s:%s', host, port)
+})
