fea6f9eacd2e7b7554b8c2189e06aa7b1bc13510
Feng <ingf.ivan@gmail.com>
2017-08-24 09:23:45 +0800

    :sparkles: add prettier

--- src/schema/schema2.js
+++ src/schema/schema2.js
@@ -1,11 +1,6 @@
-// This example demonstrates a simple server with some
-// relational data: Posts and Authors. You can get the
-// posts for a particular author, and vice-versa
 
-// Read the complete docs for graphql-tools here:
-// http://dev.apollodata.com/tools/graphql-tools/generate-schema.html
-
-import { find, filter } from 'lodash'
+import { find, filter }
+ from 'lodash'
 
 import { makeExecutableSchema } from 'graphql-tools'
 
